f120e9261187707e579e5d4da3199a2c2b0a4eaf
kyleyiyang <kyleyiyang@gmail.com>
2020-11-01 08:48:57 -0500

    Update maxPower.java

--- maxPower.java
+++ maxPower.java
@@ -2,21 +2,25 @@ class Solution {
     public int maxPower(String s) {
         if (s.length()==1) return 1;
         else {
-        ArrayList<Integer> out = new ArrayList<>();
+        //ArrayList<Integer> out = new ArrayList<>();
+        int max=0;
         for (int i=0;i<s.length()-1;i++) {
             int count = 1;
             while (i<s.length()-1 && s.charAt(i)==s.charAt(i+1)) {
                 count++;
                 i++;
             }
-            out.add(count);
+            //out.add(count);
+            if (count>max) {
+                max=count;
+            }
         }
-        int max=0;
-        for (int a:out) {
+        
+        /*for (int a:out) {
             if (a>max) {
                 max=a;
             }
-        }
+        }*/
         return max;
         }
     }
