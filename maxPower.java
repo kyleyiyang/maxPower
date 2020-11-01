e5bbbeae68e2b53f0b8e2724ff7f2fae48941ac9
kyleyiyang <kyleyiyang@gmail.com>
2020-11-01 08:40:13 -0500

    Create maxPower.java

---
+++ maxPower.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxPower(String s) {
+        if (s.length()==1) return 1;
+        else {
+        ArrayList<Integer> out = new ArrayList<>();
+        for (int i=0;i<s.length()-1;i++) {
+            int count = 1;
+            while (i<s.length()-1 && s.charAt(i)==s.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            out.add(count);
+        }
+        int max=0;
+        for (int a:out) {
+            if (a>max) {
+                max=a;
+            }
+        }
+        return max;
+        }
+    }
+}
